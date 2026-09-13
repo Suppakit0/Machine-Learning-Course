f24917a5dad2ae3fc6e1f939c71b8e5931b4f742
Suppakit0 <kit170349@gmail.com>
2026-09-14 01:32:59 +0700

    Update preprocess.py

--- ML-CPE/LAB05/classification/preprocess.py
+++ ML-CPE/LAB05/classification/preprocess.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def preprocess_data(X):
 
     # ใช้คำสั่ง np.asarray เพื่อแปลงโครงสร้างข้อมูลต้นทาง ให้กลายเป็น NumPy Array ซึ่งเป็นฟอร์แมตมาตรฐานที่โมเดล Machine Learning ต้องการ
